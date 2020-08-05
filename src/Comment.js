dc5175d903f1d085077cac06c0eb2eafe999c947
gladkill <gladkillmusic@gmail.com>
2020-08-04 20:19:04 -0700

    converted comments to Cards

--- src/Comment.js
+++ src/Comment.js
@@ -1,20 +1,34 @@
 import React from "react";
+import './Comment.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 //renders individual comment card, with  delete comment functionality
 
 function Comment({ postId, commentId, text, deleteComment, datePosted }) {
 
 
-  function handleDelete(){
+  function handleDelete() {
     deleteComment(postId, commentId)
   }
 
   console.log(datePosted)
 
 
-  return(
-    <li id={commentId}>{text} {datePosted}
-      <button onClick={handleDelete}>X</button>
+  return (
+    <li id={commentId}>
+      <Card>
+        <Card.Header>
+        {datePosted}
+        </Card.Header>
+        <Card.Body className="text-left">
+        {text} 
+        <Button onClick={handleDelete}>X</Button>
+
+        </Card.Body>
+
+
+      </Card>
     </li>
   );
 }
